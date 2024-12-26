9b736589732ea880c78ee3a1b0d3b59e1677e929
j-dmarks <jmarks@booksys.com>
2024-12-26 10:08:25 -0600

    Add initial project files and configurations for Flutter application

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'password_provider.dart';// Import the PassWord page
+import 'package:updat/updat.dart';
 import 'theme_provider.dart';// Import the ThemeProvider class
 import 'checksum.dart';
 import 'pass_word.dart'; 
@@ -37,6 +38,7 @@ class MyApp extends StatelessWidget {
         appBarTheme: const AppBarTheme(color: Colors.lightBlue),
         
       ),
+      
       darkTheme: ThemeData(
         brightness: Brightness.dark,
         primarySwatch: Colors.blue,
@@ -118,6 +120,19 @@ class HomePageState extends State<HomePage> {
         onPressed: () => _scaffoldKey.currentState?.openDrawer(),
       ),
       actions: [
+        UpdatWidget(
+          currentVersion: "1.0.1",
+          getLatestVersion: () async {
+            // Here you should fetch the latest version. It must be semantic versioning for update detection to work properly.
+            return _appVersion;
+          },
+          getBinaryUrl: (latestVersion) async {
+            // Here you provide the link to the binary the user should download. Make sure it is the correct one for the platform!
+            return "https://github.com/latest/release/bin.exe";
+          },
+          // Lastly, enter your app name so we know what to call your files.
+          appName: "Updat Example",
+        ),
         IconButton(
           icon: Icon(themeProvider.themeMode == ThemeMode.light
               ? Icons.dark_mode
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,18 @@ import Foundation
 
 import hotkey_manager_macos
 import package_info_plus
+import path_provider_foundation
+import screen_retriever
 import shared_preferences_foundation
+import url_launcher_macos
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   HotkeyManagerMacosPlugin.register(with: registry.registrar(forPlugin: "HotkeyManagerMacosPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
